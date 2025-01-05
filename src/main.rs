ce76b49a3c839bbdfb430f94773468175380daaf
Wolfyxon <wolfyxon@gmail.com>
2025-01-05 23:59:59 +0100

    init playback

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 #![windows_subsystem = "windows"]
 
+use std::{thread, time::Duration};
+
+use soloud::{audio, AudioExt, LoadExt, Soloud};
+
 fn main() {
+    let sl = Soloud::default().unwrap();
+    let mut wav = audio::Wav::default();
+
+    wav.load_mem(include_bytes!("./ping.ogg")).unwrap();
     
+    loop {
+        thread::sleep(Duration::from_secs_f32(2.0));
+        sl.play(&wav);
+    };
 }
